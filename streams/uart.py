bb1a9bcad5f7738f591eb86751c5d839982f7449
Dave Berkeley <dave@rotwang.co.uk>
2025-10-03 18:33:29 +0100

    Added ClockGen to uart

--- streams/uart.py
+++ streams/uart.py
@@ -4,6 +4,31 @@ from amaranth import *
 from streams import Stream
 
 #
+#   Simple Clock generator : finds nearest dividor of the sys_ck
+
+class ClockGen(Elaboratable):
+
+    def __init__(self, sys_ck, freq, mul=1):
+        self.o = Signal()
+        sfreq = freq * mul
+        self.idiv = int(sys_ck / sfreq)
+        self.div = Signal(range(self.idiv+1))
+        print("sample req", freq, "ck", sys_ck, 
+              "idiv", self.idiv, 
+              "mul", mul, 
+              "sample", sys_ck / (self.idiv * mul))
+
+    def elaborate(self, _):
+        m = Module()
+        m.d.sync += self.div.eq(self.div + 1)
+        with m.If(self.div == (self.idiv-1)):
+            m.d.sync += self.div.eq(0)
+
+        m.d.sync += self.o.eq(self.div == 0)
+
+        return m
+
+#
 #   Simple Tx Stream to UART
 
 class UART_Tx(Elaboratable):
